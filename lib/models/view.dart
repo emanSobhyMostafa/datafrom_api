b144c6f135de3da379936729373631b7d2a84f38
FlutLab.io <>
2021-03-29 11:20:04 +0000

    Autosave

--- lib/models/view.dart
+++ lib/models/view.dart
@@ -1,12 +1,12 @@
 class Movie {
-  int rating;
+  int albumId;
   int id;
   String title;
   String url;
   String image;
-  Movie({this.image, this.url, this.rating, this.id, this.title});
+  Movie({this.image, this.url, this.albumId, this.id, this.title});
 
   factory Movie.fromJson(Map<String, dynamic> json) {
-    return Movie(rating: json['rating'], id: json['id'], title: json['title'], url: json['url'], image: json['image']);
+    return Movie(albumId: json['albumId'], id: json['id'], title: json['title'], url: json['url'], image: json['image']);
   }
 }
