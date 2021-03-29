98c8f23b6d576c42a77ba0622914e5f2b1f5a6a2
FlutLab.io <>
2021-03-29 18:26:11 +0000

    Autosave

--- lib/reposatiry/movie_reposatiry.dart
+++ lib/reposatiry/movie_reposatiry.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../models/view.dart';
 import '../provider/movie_provider.dart';
+import '../services/movie_service.dart';
+
 class MovieReposatry {
   Movieservice _Movieservice = Movieservice();
   Future<List<Movie>> fetchMovies() {
