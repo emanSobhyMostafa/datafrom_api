25ff56efc41533b37ff9a6a1101da11e0558e982
FlutLab.io <>
2021-03-29 21:58:49 +0000

    Autosave

--- lib/ui/movie_widget.dart
+++ lib/ui/movie_widget.dart
@@ -22,13 +22,7 @@ class Moviewidget extends StatelessWidget {
                   textAlign: TextAlign.center,
                 )),
               ),
-              Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Image.network(
-                    movie.backdrop_path,
-                    height: 100.0,
-                    width: 100.0,
-                  )),
+              Padding(padding: const EdgeInsets.all(8), child: Image.network(movie.backdrop_path, height: 100.0, width: 100.0, color: Colors.black)),
             ])));
   }
 }
