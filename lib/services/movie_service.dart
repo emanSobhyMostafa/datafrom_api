13b1d4b4965f74eb83852fde227b4f61879b8645
FlutLab.io <>
2021-03-29 19:23:13 +0000

    Autosave

--- lib/services/movie_service.dart
+++ lib/services/movie_service.dart
@@ -6,7 +6,7 @@ import 'dart:convert';
 
 class Movieservice {
   Future<List<Movie>> fetchMovies() async {
-    final response = await http.get(Uri.parse('https://api.androidhive.info/json/movies.json'));
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
     if (response.statusCode == 200) {
       print(response.body);
       return List<Movie>.from((json.decode(response.body) as List).map((e) => Movie.fromJson((e)))).toList();
