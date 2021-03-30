401e26515fab56ef97741aac578536a6f0da912b
FlutLab.io <>
2021-03-30 10:26:53 +0000

    Autosave

--- lib/ui/movie_detail.dart
+++ lib/ui/movie_detail.dart
@@ -25,6 +25,6 @@ class Moviedetail extends StatelessWidget {
                 textAlign: TextAlign.center,
               ),
             ]),
-             IconButton(icon: Icon(Icons.bluetooth))));
+             )));
   }
 }
